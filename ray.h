6fb54c6ceb76a23d7a93afde26b4e8b7ce1fe886
SanaJallouli <59178137+SanaJallouli@users.noreply.github.com>
2023-12-19 20:45:20 -0800

    red circle

---
+++ ray.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray
+{
+
+public:
+	// what defines the ray is it s origin and direction 
+	point3 orig; 
+	vec3 dir;
+
+	ray() {};
+	ray(point3 origin, vec3 direction) : orig{ origin }, dir{ direction }{};
+
+	point3 origin() const { return orig; }
+	vec3 direction() const { return dir; }
+
+	point3 at(double t) { // moves the point along the ray : what color is seen along the ray 
+		return orig + t * dir; 
+	}
+
+};
+
